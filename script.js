27562349ef3b110fa3e84a4f03a22122b6f7f42d
ClementMonard <clement.monard@hotmail.fr>
2018-06-23 12:41:18 +0200

    Changer la couleur du texte en fonction du bouton choisi

---
+++ script.js
@@ -0,0 +1,15 @@
+//Déclaration de la fonction pour changer la couleur du texte en vert
+function greenColor(){
+  //Permet de changer la couleur du texte en vert
+  document.getElementById('text').style.color = 'green';
+}
+//Déclaration de la fonction pour changer la couleur du texte en rouge
+function redColor(){
+  //Permet de changer la couleur du texte en rouge
+  document.getElementById('text').style.color = 'red';
+}
+//Déclaration de la fonction pour changer la couleur du texte en bleu
+function blueColor(){
+  //Permet de changer la couleur du texte en bleu
+  document.getElementById('text').style.color = 'blue';
+}
